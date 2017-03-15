6d31b067086d7311b09bff781155a28bffef0e0a
974153457 <lixingjiuca@outlook.com>
2017-03-15 16:45:23 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 db_config = {'user': 'ece1779',
              'password': 'secret',
-             'host': '54.234.93.241',
+             'host': '54.164.167.212',
              'database': 'ece1779'}
 
 ami_id = 'ami-e3f432f5'
